2695c0373c992a12ead9eac4372abebf53c6519d
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2022-02-10 16:54:38 -0800

    more progress, added moment to format time

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 ```js
 const Ohmrrr = {
-  name: 'Omar',
+  name: Omar,
   bio: null,
+  followers: 1,
+  following: 3,
+  created: 08 /19/21,
+};
+```
 
-};```
+### Note
+
+This is inspired from something I found on another person's profile. It is also a quick little test with TypeScript, setting it up and everything.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fetchData from "./fetchData";
 import dedent from "dedent";
+import moment from 'moment';
 import * as path from "path";
 import * as fs from "fs";
 
@@ -7,16 +8,19 @@ const readMe = path.join(__dirname, "..", "README.md");
 
 const updateFile = async () => {
   const data = await fetchData("https://api.github.com/users/Ohmrrr");
+  console.log(data);
 
   const text = dedent`## Hello There 👋
   
   \`\`\`js
   const Ohmrrr = {
-    name: 'Omar',
+    name: ${data.name},
     bio: ${data.bio},
-    
-
-  };\`\`\``;
+    followers: ${data.followers},
+    following: ${data.following},
+    created: ${moment(data.created_at).format('MM/DD/YY')},
+  };
+  \`\`\``;
 
   fs.writeFileSync(readMe, text);
 };
