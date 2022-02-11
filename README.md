6492877a2633303706e92c65339a9a2e498a909c
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2022-02-10 21:52:44 -0800

    added some stat cards from https://github.com/anuraghazra/github-readme-stats

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-## Hello There 👋
+## Hello There 👋, I'm Ohmrrr
+
+I'm slightly new to GitHub but I'm on here to improve my skills and share my work.
+I primarily use JavaScript and TypeScript, but I'm still learning. So far, I've worked on:
+
+- [eTOA-001](https://github.com/Ohmrrr/etoa) - a Discord bot made with Discord.js v13. 
+
+[![eTOA](https://github-readme-stats.vercel.app/api/pin/?username=Ohmrrr&repo=etoa&show_owner=true&theme=dark)](https://github.com/anuraghazra/github-readme-stats)
+
+Hopefully there'll be more in the future.
+
+## My Stats
+
+[![My Stats](https://github-readme-stats.vercel.app/api?username=Ohmrrr&layout=compact&theme=dark)](https://github.com/anuraghazra/github-readme-stats)
 
 ```js
 const Ohmrrr = {
@@ -6,10 +19,6 @@ const Ohmrrr = {
   bio: null,
   followers: 1,
   following: 3,
-  created: 08 /19/21,
+  created: 08/19/21,
 };
 ```
-
-### Note
-
-This is inspired from something I found on another person's profile. It is also a quick little test with TypeScript, setting it up and everything.
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
-import fetchData from "./fetchData";
-import dedent from "dedent";
+import fetchData from './fetchData';
+import dedent from 'dedent';
 import moment from 'moment';
-import * as path from "path";
-import * as fs from "fs";
+import * as path from 'path';
+import * as fs from 'fs';
 
-const readMe = path.join(__dirname, "..", "README.md");
+const readMe = path.join(__dirname, '..', 'README.md');
 
 const updateFile = async () => {
-  const data = await fetchData("https://api.github.com/users/Ohmrrr");
-  console.log(data);
+  const data = await fetchData('https://api.github.com/users/Ohmrrr');
 
-  const text = dedent`## Hello There 👋
+  const text = dedent`## Hello There 👋, I'm Ohmrrr
+
+  I'm slightly new to GitHub but I'm on here to improve my skills and share my work.
+  I primarily use JavaScript and TypeScript, but I'm still learning. So far, I've worked on:
+
+  - [eTOA-001](https://github.com/Ohmrrr/etoa) - a Discord bot made with Discord.js v13. 
+  
+  [![eTOA](https://github-readme-stats.vercel.app/api/pin/?username=Ohmrrr&repo=etoa&show_owner=true&theme=dark)](https://github.com/anuraghazra/github-readme-stats)
+
+  Hopefully there'll be more in the future.
+
+  ## My Stats
+
+  [![My Stats](https://github-readme-stats.vercel.app/api?username=Ohmrrr&layout=compact&theme=dark)](https://github.com/anuraghazra/github-readme-stats)
   
   \`\`\`js
   const Ohmrrr = {
